54b3affa3481baaeca10cc02bf076368dbc37a62
teahel <teahel@163.com>
2019-10-23 18:02:22 +0800

    测试学习

--- blog_web/src/components/HelloWorld.vue
+++ blog_web/src/components/HelloWorld.vue
@@ -1,6 +1,16 @@
 <template>
   <div class="hello">
     <h1>{{ msg }}</h1>
+    <h2>{{show}}</h2>
+    <h3>{{count()}}</h3>
+    <div v-html="message"></div>
+    {{1+10}}<br>
+    {{ok?'YES':'NO'}}<br>
+    {{message.split('').reverse().join("")}}
+    <div v-bind:id="'list-'+id">测试教程</div>
+    <p  v-if="seen">现在你看到我了！</p>
+    <pre><a v-bind:href="url">gitee.com</a></pre>
+
   </div>
 </template>
 
@@ -9,9 +19,20 @@ export default {
   name: 'HelloWorld',
   data () {
     return {
-      msg: 'Welcome to Your Vue.js App'
+        msg: 'Welcome to Your Vue.js App',
+        show:'kiahis',
+        ok:true,
+        message:'li.teahel@gmail.com',
+        id:1,
+        seen:false,
+        url:'https://gitee.com'
     }
-  }
+  },
+    methods:{
+      count:function(){
+          return this.show +" - 测试的世界真是美滋滋"
+      }
+    }
 }
 </script>
 
