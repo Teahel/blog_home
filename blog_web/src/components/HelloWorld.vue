d66b93e5c6cdbd442174e3d0e5446a8c726fe06e
teahel <teahel@163.com>
2019-11-07 18:18:11 +0800

    添加使用elementUI使用

--- blog_web/src/components/HelloWorld.vue
+++ blog_web/src/components/HelloWorld.vue
@@ -1,27 +1,15 @@
 <template>
   <div class="hello">
-    <h1>{{ msg }}</h1>
-    <h2>{{show}}</h2>
-    <h3>{{count()}}</h3>
-    <div v-html="message"></div>
-    {{1+10}}<br>
-    {{ok?'YES':'NO'}}<br>
-    {{message.split('').reverse().join("")}}
 
-    <div v-bind:id="'list-'+id">测试教程</div>
-    <p  v-if="seen">现在你看到我了！</p>
-    <pre><a v-bind:href="url">gitee.com</a></pre>
-    <h6>    <a v-on:click="doSomething">点击事件</a>    </h6>
-
-    <el-container>
-      <el-aside width="200px">Aside</el-aside>
-      <el-container>
-        <el-header>Header</el-header>
-        <el-main>Main</el-main>
-        <el-footer>Footer</el-footer>
-      </el-container>
-    </el-container>
-
+    <el-card class="box-card">
+      <div slot="header" class="clearfix">
+        <span>卡片名称</span>
+        <el-button style="float: right; padding: 3px 0" type="text">操作按钮</el-button>
+      </div>
+      <div v-for="o in 4" :key="o" class="text item">
+        {{'列表内容 ' + o }}
+      </div>
+    </el-card>
 
   </div>
 
@@ -32,13 +20,7 @@ export default {
   name: 'HelloWorld',
   data () {
     return {
-        msg: 'Welcome to Your Vue.js App',
-        show:'kiahis',
-        ok:true,
-        message:'li.teahel@gmail.com',
-        id:1,
-        seen:false,
-        url:'https://gitee.com'
+
     }
   },
     methods:{
@@ -55,43 +37,44 @@ export default {
                 .catch(function (error) {
                     console.log(error);
                 });
-        }
+        },
+
+    },
+    created() {
+       this.$axios.get("http://39.108.125.164:8080/core_start/user/login",{params:{username:'matthew',password:'1996'}}) .then(function (response) {
+            if(response.data!=null){
+                alert(response.data.msg+"  "+response.data.code);
+            }
+            console.log(response);
+        })
+            .catch(function (error) {
+                console.log(error);
+            });
+
     }
+
 }
 </script>
 
 <style scoped>
-  .el-header, .el-footer {
-    background-color: #B3C0D1;
-    color: #333;
-    text-align: center;
-    line-height: 60px;
+  .text {
+    font-size: 14px;
   }
 
-  .el-aside {
-    background-color: #D3DCE6;
-    color: #333;
-    text-align: center;
-    line-height: 200px;
+  .item {
+    margin-bottom: 18px;
   }
 
-  .el-main {
-    background-color: #E9EEF3;
-    color: #333;
-    text-align: center;
-    line-height: 160px;
+  .clearfix:before,
+  .clearfix:after {
+    display: table;
+    content: "";
+  }
+  .clearfix:after {
+    clear: both
   }
 
-  body > .el-container {
-    margin-bottom: 40px;
-  }
-
-  .el-container:nth-child(5) .el-aside,
-  .el-container:nth-child(6) .el-aside {
-    line-height: 260px;
-  }
-
-  .el-container:nth-child(7) .el-aside {
-    line-height: 320px;
+  .box-card {
+    width: 480px;
   }
 </style>
--- blog_web/src/main.js
+++ blog_web/src/main.js
@@ -6,6 +6,7 @@ import router from './router'
 import axios from 'axios'
 import ElementUI from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
+import 'element-ui/lib/theme-chalk/display.css';
 
 Vue.prototype.$axios = axios    //全局注册，使用方法为:this.$axios
 Vue.config.productionTip = false
