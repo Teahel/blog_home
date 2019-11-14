84d801d84fe91bfeb48da6705dca43d8f6c4e539
teahel <teahel@163.com>
2019-11-14 15:46:22 +0800

    修改使用查询数据

--- blog_web/src/components/HelloWorld.vue
+++ blog_web/src/components/HelloWorld.vue
@@ -1,15 +1,40 @@
 <template>
-  <div class="hello">
+  <div class="hello" >
 
-    <el-card class="box-card">
-      <div slot="header" class="clearfix">
-        <span>卡片名称</span>
-        <el-button style="float: right; padding: 3px 0" type="text">操作按钮</el-button>
-      </div>
-      <div v-for="o in 4" :key="o" class="text item">
-        {{'列表内容 ' + o }}
-      </div>
-    </el-card>
+    <el-container height=80% direction="vertical">
+      <el-header>Header</el-header>
+      <el-container height="100%">
+        <el-aside width="200px">
+          <el-button type="danger" @click="show">危险按钮</el-button>
+        </el-aside>
+        <el-container >
+          <el-main >
+            <el-table
+              :data="tableData"
+              stripe
+              style="width: 100%">
+              <el-table-column
+                prop="title"
+                label="标题"
+                width="180">
+              </el-table-column>
+              <el-table-column
+                prop="content"
+                label="内容"
+                width="180">
+              </el-table-column>
+              <el-table-column
+                prop="reprintlink"
+                label="连接">
+              </el-table-column>
+            </el-table>
+
+          </el-main>
+          <el-footer>Footer</el-footer>
+        </el-container>
+      </el-container>
+    </el-container>
+
 
   </div>
 
@@ -20,13 +45,10 @@ export default {
   name: 'HelloWorld',
   data () {
     return {
-
+        tableData:[],
     }
   },
     methods:{
-        count:function(){
-          return this.show +" - 测试的世界真是美滋滋"
-        },
         doSomething:function () {
            return  this.$axios.get("http://39.108.125.164:8080/core_start/user/login",{params:{username:'matthew',password:'1996'}}) .then(function (response) {
                if(response.data!=null){
@@ -38,18 +60,18 @@ export default {
                     console.log(error);
                 });
         },
-
-    },
-    created() {
-       this.$axios.get("http://39.108.125.164:8080/core_start/user/login",{params:{username:'matthew',password:'1996'}}) .then(function (response) {
-            if(response.data!=null){
-                alert(response.data.msg+"  "+response.data.code);
-            }
-            console.log(response);
+        show:function () {
+            let datas = this;
+            this.$axios.post("http://39.108.125.164:8080/core_start/article/findList") .then(function (response) {
+                if(response.data!=null){
+                    console.log(response.data.list);
+                    datas.tableData = response.data.list;
+                }
             })
-            .catch(function (error) {
-                console.log(error);
-            });
+                .catch(function (error) {
+                    console.log(error);
+                });
+        }
 
     }
 
@@ -57,24 +79,46 @@ export default {
 </script>
 
 <style scoped>
-  .text {
-    font-size: 14px;
+
+  .el-header, .el-footer {
+    background-color: #B3C0D1;
+    color: #333;
+    text-align: center;
+    line-height: 60px;
+    height: 200px;
+
   }
 
-  .item {
-    margin-bottom: 18px;
+  .el-aside {
+    background-color: #D3DCE6;
+    color: #333;
+    text-align: center;
+    line-height: 200px;
   }
 
-  .clearfix:before,
-  .clearfix:after {
-    display: table;
-    content: "";
-  }
-  .clearfix:after {
-    clear: both
+  .el-main {
+    background-color: #E9EEF3;
+    color: #333;
+    text-align: center;
+    line-height: 160px;
   }
 
-  .box-card {
-    width: 480px;
+  body > .el-container {
+    height:100%;
+    padding:0;
+    margin:0;
+    width:100%;
+    margin-bottom: 5px;
   }
+
+  .el-container:nth-child(5) .el-aside,
+  .el-container:nth-child(6) .el-aside {
+    line-height: 260px;
+  }
+
+  .el-container:nth-child(7) .el-aside {
+    line-height: 320px;
+  }
+
+
 </style>
